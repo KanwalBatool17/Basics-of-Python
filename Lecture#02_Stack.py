cc916e4c76b53e36a225150c39a1906022999e89
KanwalBatool17 <kanwalbatool579@gmail.com>
2020-08-26 20:13:46 +0500

    1-Learning the method of Stack in python programming. 2-Using key-word (.append) and (.pop)

---
+++ Lecture#02_Stack.py
@@ -0,0 +1,26 @@
+print("1- Upload file on Github from Python \n")
+print("2- Learning the method of Stack in Python Programming \n")
+
+Fruit_List=[]
+Fruit_List.append("Plum")
+Fruit_List.append("Apricot")
+Fruit_List.append("BlueBarry")
+Fruit_List.append("Grapes")
+Fruit_List.append("Cucumber")
+print(Fruit_List)
+
+
+Fruit_List.pop()
+print(Fruit_List)
+
+Fruit_List.pop()
+print(Fruit_List)
+
+Fruit_List.pop()
+print(Fruit_List)
+
+Fruit_List.pop()
+print(Fruit_List)
+
+Fruit_List.pop()
+print(Fruit_List)
