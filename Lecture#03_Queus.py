a6460067ec695e74e42e62219fc8d315246d8975
KanwalBatool17 <kanwalbatool579@gmail.com>
2020-09-02 23:02:07 +0500

    1-sorting list with for loop 2- sorting list with while loop

---
+++ Lecture#03_Queus.py
@@ -0,0 +1,41 @@
+print(" --------------------- For Loop-------------------------------")
+
+def sort(List_num):
+
+    for i in range (0,2):
+        mini = i
+        for j in range (i,3):
+            if List_num [j] < List_num [mini]:
+                mini = j
+
+        temp = List_num [i]
+        List_num [i]= List_num [mini]
+        List_num [mini] = temp
+
+        print(List_num)
+
+List_num = [8,5,2]
+sort(List_num)
+
+print(List_num)
+
+
+
+print("_______________ While Loop___________________________")
+
+
+list =[1,3,4,5,7]
+i=0
+temp= 0
+while i < len(list):
+    j = i + 1
+    while j < len(list):
+        if list[i] > list[j]:
+            temp = list[i]
+            list[i]=list[j]
+            list[j]=temp
+        j = j + 1
+    i = i + 1
+print(list)
+
+
